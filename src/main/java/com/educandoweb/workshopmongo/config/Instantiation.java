cdf54d21529295b5963da937f8323d187784041b
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-11-05 12:55:43 -0300

    9-Criando entity Post com User aninhado

--- src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -1,13 +1,16 @@
 package com.educandoweb.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.repositories.PostRepository;
 import com.educandoweb.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -15,11 +18,16 @@ public class Instantiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository useRepository;
+	@Autowired
+	private PostRepository postRepository; 
 	
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		useRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -27,5 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		useRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post3 = new Post(null, sdf.parse("25/03/2018"), "Boa noite", "Faremos happyhour hoje as 17:00hs. Abraços!", alex);
+		Post post4 = new Post(null, sdf.parse("27/03/2018"), "Beleza Galera", "Faremos almoco de despedida para Jhon hoje. Abraços!", bob);
+		
+		postRepository.saveAll(Arrays.asList(post1,post2,post3,post4));
 	}
 }
